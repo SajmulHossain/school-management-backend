6766333eee82a220637ef6c7818f7f71cffb279a
Sajmul <sajmul1427@gmail.com>
2025-08-02 12:05:29 +0600

    server handled done

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,66 @@ const pushStartServer = async() => {
     })
 }
 
+
 pushStartServer();
+
+process.on("SIGINT", () => {
+    console.log("SIGINT detected. Server is going down...");
+
+    if(server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM detected. Server is going down...");
+
+    if(server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
+
+process.on("unhandledRejection", () => {
+    console.log('UnhandleRejection detected. Server is going down...');
+
+    if(server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);    
+})
+
+process.on("uncaughtException", () => {
+    console.log("uncaughtException detected. Server is going down...");
+
+    if(server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);    
+})
+
+process.on("", () => {
+    console.log("uncaughtException detected. Server is going down...");
+
+    if(server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);    
+})
+
