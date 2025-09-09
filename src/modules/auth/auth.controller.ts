a84579553043c8f0406bcd4722a4a904b989fa59
Sajmul <sajmul1427@gmail.com>
2025-09-09 20:44:40 +0600

    cookie set after login and register

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,49 +5,56 @@ import { AuthService } from "./auth.service";
 import { User } from "../user/user.model";
 import { AppError } from "../../utils/AppError";
 import { compare } from "bcryptjs";
+import { generateCookie, setCookie } from "../../utils/jwt&cookie/cookie";
 
-const register = catchAsync(async(req:Request, res: Response) => {
-    const data = await AuthService.register(req.body);
+const register = catchAsync(async (req: Request, res: Response) => {
+  const data = await AuthService.register(req.body);
+  const cookie = generateCookie(data);
+  setCookie(res, cookie);
+  sendResponse(res, {
+    message: "Registration Successful",
+    data,
+    statusCode: 201,
+  });
+});
 
-    sendResponse(res, {
-        message: 'Registration Successful',
-        data,statusCode:201
-    })
-})
+const login = catchAsync(async (req: Request, res: Response) => {
+  const { email, password, phone } = req.body;
 
-const login = catchAsync(async(req: Request, res: Response) => {
-    const { email, password, phone } = req.body;
+  const auth = email || phone;
 
-    const auth = email || phone;
+  if (!auth) {
+    throw new AppError(400, "Please give your email or number");
+  }
 
-    if(!auth) {
-        throw new AppError(400, 'Please give your email or number')
-    }
-    
-    const isUserExist = await User.isUserExist(auth);
-    
-    if(!isUserExist) {
-        throw new AppError(404, 'No account found. Create a new account');
-    }
+  const isUserExist = await User.isUserExist(auth);
 
-    const isPasswordMatched = await compare(password, isUserExist.password as string);
+  if (!isUserExist) {
+    throw new AppError(404, "No account found. Create a new account");
+  }
 
-    if(!isPasswordMatched) {
-        throw new AppError(401, 'Invalid credentials');
-    }
+  const isPasswordMatched = await compare(
+    password,
+    isUserExist.password as string
+  );
 
-   isUserExist.password = undefined;
+  if (!isPasswordMatched) {
+    throw new AppError(401, "Invalid credentials");
+  }
 
-   
+  isUserExist.password = undefined;
 
-    sendResponse(res, {
-        message: 'Log in successfull',
-        statusCode: 200,
-        data: isUserExist
-    })
-})
+  const cookie = generateCookie(isUserExist);
+  setCookie(res, cookie);
+
+  sendResponse(res, {
+    message: "Log in successfull",
+    statusCode: 200,
+    data: isUserExist,
+  });
+});
 
 export const AuthController = {
-    register,
-    login
-}
+  register,
+  login,
+};
